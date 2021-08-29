500817087f096bd1983a67487b68faf80a525284
Teimur Gasanov <teymurgg321@gmail.com>
2021-08-29 13:42:49 +0600

    Wait for assets

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,31 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const loadAssets = () => {
+  return new Promise<void>((resolve) => {
+    document.addEventListener("DOMContentLoaded", () => {
+      const documentAny = document as any;
+
+      if (documentAny.fonts) {
+        documentAny.fonts.ready.then(resolve);
+      } else {
+        resolve();
+      }
+    });
+  });
+};
+
+const renderApp = () =>
+  loadAssets().then(() => {
+    ReactDOM.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+      document.getElementById("root")
+    );
+  });
+
+renderApp();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
